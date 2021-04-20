edaa8ff35f3eb14e2031bce45eb39bd5df68697f
tujiuquan <m18646088512@163.com>
2021-04-21 04:51:04 +0800

    调整注入方式

--- dynamicmultipledatasources-demo/src/main/java/com/luoyu/dynamicmultipledatasources/controller/UserController.java
+++ dynamicmultipledatasources-demo/src/main/java/com/luoyu/dynamicmultipledatasources/controller/UserController.java
@@ -3,11 +3,14 @@ package com.luoyu.dynamicmultipledatasources.controller;
 import com.luoyu.dynamicmultipledatasources.entity.User;
 import com.luoyu.dynamicmultipledatasources.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author luoyu
@@ -16,8 +19,12 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class UserController {
 
+    private final IUserService iUserService;
+
     @Autowired
-    private IUserService iUserService;
+    public UserController(IUserService iUserService) {
+        this.iUserService = iUserService;
+    }
 
     /**
      * 查询db1
