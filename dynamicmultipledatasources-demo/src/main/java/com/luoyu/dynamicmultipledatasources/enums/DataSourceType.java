86d41257f693246acd2bad136cefe06943ef7506
tujiuquan <m18646088512@163.com>
2021-04-21 04:50:34 +0800

    添加枚举注解

--- dynamicmultipledatasources-demo/src/main/java/com/luoyu/dynamicmultipledatasources/enums/DataSourceType.java
+++ dynamicmultipledatasources-demo/src/main/java/com/luoyu/dynamicmultipledatasources/enums/DataSourceType.java
@@ -2,9 +2,16 @@ package com.luoyu.dynamicmultipledatasources.enums;
 
 /**
  * 枚举类，用于选择特定的数据源
+ * @author quange
  */
 public enum DataSourceType {
-
-    DB1, DB2
+    /**
+     * 数据源1
+     */
+    DB1,
+    /**
+     * 数据源2
+     */
+    DB2
 
 }
